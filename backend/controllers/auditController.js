48b12e5df7b591c29cf479a315b1e063b124b14a
mayurkanade44 <mayurkanade44@gmail.com>
2022-09-24 14:34:09 +0530

    backend audit file generation

--- backend/controllers/auditController.js
+++ backend/controllers/auditController.js
@@ -1,4 +1,5 @@
 import Audit from "../models/Audit.js";
+import exceljs from "exceljs";
 
 export const auditReport = async (req, res) => {
   const { restroName, restroManger, restroEmail, auditList, restaurant } =
@@ -15,7 +16,15 @@ export const auditReport = async (req, res) => {
       return res.status(400).json({ msg: "Please provide all values" });
     }
 
-    const auidtReport = await Audit.create(req.body);
+    const auditReport = await Audit.create(req.body);
+    const error1 = await auditFile(auditReport);
+    if (error1) {
+      return res
+        .status(400)
+        .json({
+          msg: "There is some error while generating a file. Try again later",
+        });
+    }
     res
       .status(201)
       .json({ msg: `${restroName} audit report has been created` });
@@ -24,5 +33,73 @@ export const auditReport = async (req, res) => {
     return res
       .status(500)
       .json({ msg: "Something went wrong, please try again later" });
+  }
+};
+
+const auditFile = async (auditReport) => {
+  const data = auditReport.auditList;
+  const filename = auditReport.restroName;
+  const row2 = `Store Code : ${auditReport.restroName}            Store Manager : ${auditReport.restroManger}`;
+
+  try {
+    const workbook = new exceljs.Workbook();
+
+    await workbook.xlsx.readFile("book1.xlsx");
+    let worksheet = workbook.getWorksheet("Sheet1");
+
+    let row = worksheet.getRow(2);
+    row.getCell(1).value = row2;
+
+    data.map((item, index) => {
+      let row = worksheet.getRow(index + 4);
+      row.getCell(1).value = item.device;
+      row.getCell(2).value = item.condition1;
+      row.getCell(3).value = item.condition2;
+      row.getCell(4).value = item.condition3;
+      row.getCell(5).value = item.location;
+      //   row.getCell(6).value = item.picture;
+      row.alignment = { vertical: "middle", horizontal: "left" };
+      row.commit();
+    });
+
+    workbook.xlsx.writeFile(`${filename}.xlsx`);
+
+    // sheet.columns = [
+    //   { header: "Sr No", key: "no", width: 20 },
+    //   { header: "Condition 1", key: "condition1", width: 20 },
+    //   { header: "Condition 2", key: "condition2", width: 20 },
+    //   { header: "Condition 3", key: "condition3", width: 20 },
+    //   { header: "Location", key: "location", width: 20 },
+    //   { header: "Picture", key: "pic", width: 20 },
+    // ];
+
+    // await data.map((item) => {
+    //   sheet.addRow({
+    //     no: item.device,
+    //     condition1: item.condition1,
+    //     condition2: item.condition2,
+    //     condition3: item.condition3,
+    //     location: item.location,
+    //   });
+
+    //   sheet.getRow(1).style.alignment = {
+    //     vertical: "middle",
+    //     horizontal: "center",
+    //   };
+
+    //   res.setHeader(
+    //     "Content-Type",
+    //     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    //   );
+    //   res.setHeader(
+    //     "Content-Disposition",
+    //     "attachment;filename=" + "exportData.xlsx"
+    //   );
+    // });
+
+    // workbook.xlsx.writeFile("test.xlsx");
+  } catch (error) {
+    console.log(error);
+    return error;
   }
 };
--- backend/controllers/restroRegControlller.js
+++ backend/controllers/restroRegControlller.js
@@ -1,5 +1,5 @@
 import RestroRegistration from "../models/RestroReg.js";
-import exceljs from "exceljs";
+
 
 export const restroReg = async (req, res) => {
   const { restroName, restroManger, restroEmail, auditList } = req.body;
@@ -93,62 +93,3 @@ const data = [
   },
 ];
 
-const excel1 = async (req, res) => {
-  try {
-    const workbook = new exceljs.Workbook();
-    const sheet = workbook.addWorksheet("My Sheet");
-
-    await workbook.xlsx.readFile("book1.xlsx");
-    let worksheet = workbook.getWorksheet("Sheet1");
-    data.map((item, index) => {
-      let row = worksheet.getRow(index + 4);
-      row.getCell(1).value = item.name;
-      row.getCell(2).value = item.condition1;
-      row.getCell(3).value = item.condition2;
-      row.getCell(4).value = item.condition3;
-      row.getCell(5).value = item.location;
-      row.getCell(6).value = item.picture;
-      row.alignment = { vertical: "middle", horizontal: "left" };
-      row.commit();
-    });
-
-    workbook.xlsx.writeFile("5050813123.xlsx");
-
-    sheet.columns = [
-      { header: "Sr No", key: "no", width: 20 },
-      { header: "Condition 1", key: "condition1", width: 20 },
-      { header: "Condition 2", key: "condition2", width: 20 },
-      { header: "Condition 3", key: "condition3", width: 20 },
-      { header: "Location", key: "location", width: 20 },
-      { header: "Picture", key: "pic", width: 20 },
-    ];
-
-    await data.map((item) => {
-      sheet.addRow({
-        no: item.name,
-        condition1: item.condition1,
-        condition2: item.condition2,
-        condition3: item.condition3,
-        location: item.location,
-      });
-
-      sheet.getRow(1).style.alignment = {
-        vertical: "middle",
-        horizontal: "center",
-      };
-
-      // res.setHeader(
-      //   "Content-Type",
-      //   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-      // );
-      // res.setHeader(
-      //   "Content-Disposition",
-      //   "attachment;filename=" + "exportData.xlsx"
-      // );
-    });
-
-    // workbook.xlsx.writeFile("test.xlsx");
-  } catch (error) {
-    console.log(error);
-  }
-};
